be9634c20c9e0d52afac15680322bc68fcc9f5ad
unknown <joaovictorgit21@gmail.com>
2023-04-15 17:54:40 -0300

    Atividade do formulário de cadastro

---
+++ js/main.js
@@ -0,0 +1,21 @@
+        var input     = document.getElementsByTagName('input');
+        var user      = document.getElementById('user');
+        var tel       = document.getElementById('tel');
+        var date      = document.getElementById('date');
+        var cpf       = document.getElementById('cpf');
+        var bairro    = document.getElementById('bairro');
+        var rua       = document.getElementById('rua');
+        var casa      = document.getElementById('casa');
+        var cep       = document.getElementById('cep');
+        var email     = document.getElementById('email');
+        var pswrd     = document.getElementById('pswrd');
+        var pswrd2    = document.getElementById('pswrd2');
+        var btn       = document.getElementById('btn');
+
+
+function Verificar()
+{
+        if (input == "") {
+                alert("Preencha")
+        }
+}
