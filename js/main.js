de4afb01005da38f1788293b19742e506760aac3
unknown <joaovictorgit21@gmail.com>
2023-04-17 18:21:46 -0300

    Code_Atualizado

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 function Verificar()
 {
-        let input     = document.getElementsByTagName('input');
-        let user      = document.getElementById('user');
-        let tel       = document.getElementById('tel');
-        let date      = document.getElementById('date');
-        let cpf       = document.getElementById('cpf');
-        let bairro    = document.getElementById('bairro');
-        let rua       = document.getElementById('rua');
-        let casa      = document.getElementById('casa');
-        let cep       = document.getElementById('cep');
-        let email     = document.getElementById('email');
-        let pswrd     = document.getElementById('pswrd');
-        let pswrd2    = document.getElementById('pswrd2');
-        let btn       = document.getElementById('btn');
+        let input     = document.getElementsByTagName('input') ;
+        let user      = document.getElementById('user').value;
+        let tel       = document.getElementById('tel').value;
+        let date      = document.getElementById('date').value;
+        let cpf       = document.getElementById('cpf').value;
+        let bairro    = document.getElementById('bairro').value;
+        let rua       = document.getElementById('rua').value;
+        let casa      = document.getElementById('casa').value;
+        let cep       = document.getElementById('cep').value;
+        let email     = document.getElementById('email').value;
+        let pswrd     = document.getElementById('pswrd').value;
+        let pswrd2    = document.getElementById('pswrd2').value;
 
-        if (pswrd != pswrd2) {
+        if (!input) {
+                alert("Preencha todos os campos");
+        } else if (pswrd != pswrd2) {
                 alert("As senhas devem ser iguais");
-                
-                return false
         }
 }
