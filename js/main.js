316891a0083f1b7b1a319d3f1a04573e2bbbee7e
unknown <joaovictorgit21@gmail.com>
2023-04-18 20:20:01 -0300

    JS Atualizado

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
+function mascara(i){
+   
+        var v = i.value;
+        
+        if(isNaN(v[v.length-1])) {
+           i.value = v.substring(0, v.length-1);
+           return;
+        }
+        
+        i.setAttribute("maxlength", "14");
+        if (v.length == 3 || v.length == 7) i.value += ".";
+        if (v.length == 11) i.value += "-";
+     
+}
+
 function Verificar()
 {
-        let input     = document.getElementsByTagName('input') ;
-        let user      = document.getElementById('user').value;
-        let tel       = document.getElementById('tel').value;
-        let date      = document.getElementById('date').value;
-        let cpf       = document.getElementById('cpf').value;
-        let bairro    = document.getElementById('bairro').value;
-        let rua       = document.getElementById('rua').value;
-        let casa      = document.getElementById('casa').value;
-        let cep       = document.getElementById('cep').value;
-        let email     = document.getElementById('email').value;
-        let pswrd     = document.getElementById('pswrd').value;
-        let pswrd2    = document.getElementById('pswrd2').value;
+        let user      = document.getElementById('user')
+        let tel       = document.getElementById('tel')
+        let date      = document.getElementById('date')
+        let cpf       = document.getElementById('cpf')
+        let bairro    = document.getElementById('bairro')
+        let rua       = document.getElementById('rua')
+        let casa      = document.getElementById('casa')
+        let cep       = document.getElementById('cep')
+        let email     = document.getElementById('email')
+        let pswrd     = document.getElementById('pswrd')
+        let pswrd2    = document.getElementById('pswrd2')
 
-        if (!input) {
+        if (!user || !tel || !date || !cpf || !bairro || !rua || !casa || !cep || !email || !pswrd || !pswrd2) {
                 alert("Preencha todos os campos");
-        } else if (pswrd != pswrd2) {
+        } 
+        else if (pswrd != pswrd2) {
                 alert("As senhas devem ser iguais");
         }
+        else if (pswrd == "123456789") {
+                alert("Senha muito óbvia")
+        }
 }
+
